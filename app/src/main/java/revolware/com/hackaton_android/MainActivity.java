774b8c60c35802ca808a83341368e04d2eae17b1
xgallom <gallo.milan.jr@gmail.com>
2017-04-07 19:55:58 +0200

    Initial commit

---
+++ app/src/main/java/revolware/com/hackaton_android/MainActivity.java
@@ -0,0 +1,13 @@
+package revolware.com.hackaton_android;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
