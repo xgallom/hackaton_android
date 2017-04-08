ba038b99e75d6a505442145d666b45be90de805d
xgallom <gallo.milan.jr@gmail.com>
2017-04-08 22:44:39 +0200

    Initiated ActivityLocation development with Jakub

---
+++ app/src/main/java/revolware/com/hackaton_android/activities/LocationActivity.java
@@ -0,0 +1,16 @@
+package revolware.com.hackaton_android.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import revolware.com.hackaton_android.R;
+
+public class LocationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_location);
+    }
+
+}
