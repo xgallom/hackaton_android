c8efa98d4b975959a6a5a2db1a973666d5f78e90
admin <jakubko.ondrusek@gmail.com>
2017-04-08 13:12:37 +0200

    fe

--- app/src/main/java/revolware/com/hackaton_android/activities/info_input.java
+++ app/src/main/java/revolware/com/hackaton_android/activities/info_input.java
@@ -1,37 +1,60 @@
 package revolware.com.hackaton_android.activities;
 
+import android.app.AlertDialog;
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Gravity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.Calendar;
 
 import revolware.com.hackaton_android.R;
 
 public class info_input extends AppCompatActivity {
-
+    Button fromButt,toButt;
+    TextView location;
+    int Num;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info_input);
-/*
+        Num=0;
 
-        RelativeLayout ln = (RelativeLayout) this.findViewById(R.id.main);
+        fromButt = (Button) findViewById(R.id.from);
+        toButt = (Button) findViewById(R.id.to);
 
-        LinearLayout linLay = new LinearLayout(this);
-        linLay.setOrientation(LinearLayout.VERTICAL);
-        linLay.setGravity(Gravity.CENTER_VERTICAL);
-        linLay.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.MATCH_PARENT, 1));
-        ln.addView(linLay);
+        fromButt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DialogFragment dialogfragment = new DatePickerDialogTheme1();
 
-        DatePicker startTime = new DatePicker(this);
-        DatePicker endTime = new DatePicker(this);
-        linLay.addView(startTime);
-        linLay.addView(endTime);
+                dialogfragment.show(getFragmentManager(), "Theme 1");
+                Num=0;
 
-*/
+            }
+        });
+
+        toButt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DialogFragment dialogfragment = new DatePickerDialogTheme2();
+                Num=1;
+
+                dialogfragment.show(getFragmentManager(), "Theme 2");
+            }
+        });
 
 
     }
+
 
     public void addView(LinearLayout main){
 
@@ -42,4 +65,60 @@ public class info_input extends AppCompatActivity {
 
         main.addView(linLay);
     }
+
+    public static class DatePickerDialogTheme1 extends DialogFragment implements DatePickerDialog.OnDateSetListener {
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            final java.util.Calendar calendar = Calendar.getInstance();
+            int year = calendar.get(Calendar.YEAR);
+            int month = calendar.get(Calendar.MONTH);
+            int day = calendar.get(Calendar.DAY_OF_MONTH);
+
+            Log.e("date", calendar.toString());
+
+            DatePickerDialog datepickerdialog = new DatePickerDialog(getActivity(),
+                    AlertDialog.THEME_DEVICE_DEFAULT_DARK, this, year, month, day);
+
+            return datepickerdialog;
+        }
+
+        public void onDateSet(DatePicker view, int year, int month, int day) {
+
+            Button fromButt = (Button) getActivity().findViewById(R.id.from);
+
+            fromButt.setText(day + "." + (month + 1) + "." + year);
+
+
+
+        }}
+
+        public static class DatePickerDialogTheme2 extends DialogFragment implements DatePickerDialog.OnDateSetListener {
+
+            @Override
+            public Dialog onCreateDialog(Bundle savedInstanceState) {
+                final java.util.Calendar calendar = Calendar.getInstance();
+                int year = calendar.get(Calendar.YEAR);
+                int month = calendar.get(Calendar.MONTH);
+                int day = calendar.get(Calendar.DAY_OF_MONTH);
+
+                Log.e("date", calendar.toString());
+
+                DatePickerDialog datepickerdialog = new DatePickerDialog(getActivity(),
+                        AlertDialog.THEME_DEVICE_DEFAULT_DARK, this, year, month, day);
+
+                return datepickerdialog;
+            }
+
+            public void onDateSet(DatePicker view, int year, int month, int day) {
+
+
+                Button toButt = (Button) getActivity().findViewById(R.id.to);
+
+                toButt.setText(day + "." + (month + 1) + "." + year);
+
+
+            }
+        }
+
 }
